7d3b5f64634adcd7b8d7be53d377885e0d6b6d38
Gowrish15 <gowrishvicky@gmail.com>
2020-11-02 09:47:11 +0530

    r

--- php/download.php
+++ php/download.php
@@ -32,12 +32,9 @@ $host='localhost';
         // Now update downloads count
         $newCount = $file['dcount'] + 1;
         $updateQuery =  mysqli_query($conn, "update resources set dcount='$newCount' WHERE id='$id'");
-        header("Location: testing.php");
-        //header("Refresh:0 ; url=testing.php");
-        
-        exit;
-        
+        //header("Location: testing.php");
+        header("Refresh:0 ; url=testing.php");
+        exit;        
     }
-
 }
 ?>
