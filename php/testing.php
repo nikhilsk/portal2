eb245efb74d5daf1044a16965235e5bc106b55af
nikhilsk <nikhils2400@gmail.com>
2020-11-06 18:35:41 +0530

    first

--- php/testing.php
+++ php/testing.php
@@ -175,7 +175,7 @@ session_start();
       rel="stylesheet"
       href="https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css"
     />
-    <link rel="stylesheet" href="../css/layout.css" />
+    <link rel="stylesheet" href="../css/layout1.css" />
     <script
       defer
       src="https://use.fontawesome.com/releases/v5.14.0/js/all.js"
